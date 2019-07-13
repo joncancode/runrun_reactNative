44f8bb7767117f1cfda578408bf6ac42e754b73c
joncancode <jc.haines19@gmail.com>
2019-07-13 17:05:35 -0400

    add map and allow permission

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Initial app creation</Text>
+import { MapView, Permissions } from 'expo'
+
+export default class App extends React.Component {
+  state = {
+    latitude: null,
+    longitude: null
+  }
+
+  async componentDidMount(){
+    const {status} = await Permissions.getAsync(Permissions.LOCATION)
+
+    if (status != 'granted'){
+      const response = await Permissions.askAsync(Permissions.LOCATION)
+    }
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
+      (error) => console.log('Error:', error)
+    )
+  }
+  render(){
+    const {
+      latitude,
+      longitude,
+    } = this.state
+
+  if (latitude){
+    return (
+      <MapView
+      style={{ flex: 1 }}
+      showsUserLocation
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.01
+      }}
+    >
+      </MapView>
+    );
+  }
+ 
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Please Allow Location</Text>
     </View>
-  );
+  )
+  }
 }
 
 const styles = StyleSheet.create({
